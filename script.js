b0ad6c580d5192bb983c58ea8e21733d7e5418ed
bishopivy <contact@bishopivy.com>
2022-05-12 17:18:07 -0400

    cleaning up script

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const headerIframe = document.getElementsByClassName('header-iframe')[0]
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const topLinks = document.getElementsByClassName('topLinks')[0]
 
 function createMenu() {
 	topLinks.classList.toggle('active');
-	bigIframe.classList.toggle('active');
 }
 
 toggleButton.addEventListener('click', createMenu)
