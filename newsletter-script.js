26bc56bc78b709d2da4b92fff087f942ec1fd22a
bishopivy <contact@bishopivy.com>
2022-05-17 14:18:04 -0400

    server side implemented

--- newsletter-script.js
+++ newsletter-script.js
@@ -7,7 +7,7 @@ const sendPostMessage = () => {
     if (newsHeight !== news.offsetHeight) {
         newsHeight = news.offsetHeight;
         window.parent.postMessage({
-        newsFrameHeight: newsHeight
+        newsFrameHeight: newsHeight + 10
         }, '*');
         //console.log(newsHeight) // check the message is being sent correctly
     }
@@ -21,38 +21,7 @@ function loadThankYou() {
     sendPostMessage();
 }
 
-function processForm(event) {
-    let valid = false;
-    const fName = (news.elements['First Name']).value;
-    const lName = (news.elements['Last Name']).value;
-    const email = (news.elements['Email']).value;
-    const isNameValid = (name) => {
-        if (name.length >= 40) { 
-            return false;
-        } else {
-            const re = new RegExp("(^(?!.*=).*$)");
-            return re.test(name);
-        }
-    }
-    const isEmailValid = (email) => {
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        const noeq = new RegExp("(^(?!.*=).*$)");
-        return re.test(email) && noeq.test(email);
-    };
-
-    if (isNameValid(fName) && isNameValid(lName) && isEmailValid(email)) {
-        valid = true;
-    }
-
-    if (valid) {
-       loadThankYou();
-    } else {
-        event.preventDefault();
-    }
-    
-}
-
 window.onload = () => sendPostMessage();
 window.onresize = () => sendPostMessage();
 
-news.addEventListener('submit', processForm);
+news.addEventListener('submit', loadThankYou);
