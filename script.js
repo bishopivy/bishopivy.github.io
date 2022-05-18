79cc7bf736381a23dab60ab1e10462dda3a780fe
bishopivy <contact@bishopivy.com>
2022-05-18 18:41:35 -0400

    better mobile button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const topLinks = document.getElementsByClassName('topLinks')[0]
 const entireHeader = document.getElementsByClassName('header-inner')[0]
+const tBar = document.getElementsByClassName('top-bar')[0]
+const bBar = document.getElementsByClassName('bottom-bar')[0]
 
 
 let headerHight;
@@ -17,6 +19,8 @@ const sendPostMessage = () => {
 function createMenu() {
 	topLinks.classList.toggle('active');
 	sendPostMessage();
+  tBar.classList.toggle('active');
+  bBar.classList.toggle('active');
 }
 
 toggleButton.addEventListener('click', createMenu);
